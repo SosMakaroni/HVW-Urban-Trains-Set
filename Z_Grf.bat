8b3b53fbd8d31d5001bd832d4dd63219f885da8e
SosMakaroni <balint0016@hotmail.com>
2024-02-16 20:19:47 +0100

    Voxelek
    Az általam módosított voxelek feltöltése

--- Z_Grf.bat
+++ Z_Grf.bat
@@ -1,7 +1,8 @@
 chcp 65001
-del "C:\Users\balin\Documents\GitHub\HVW-Urban-Trains-Set\HVWUTS.nml"
-copy /b "C:\Users\balin\Documents\GitHub\HVW-Urban-Trains-Set\Vehicles_code\*.nml" "C:\Users\balin\Documents\GitHub\HVW-Urban-Trains-Set\HVWUTS.nml"
-python3 D:\nml-master\nmlc -c --grf HVWUTS.grf HVWUTS.nml
+del "C:\Users\balin\OneDrive\Dokumentumok\GitHub\HVW-Urban-Trains-Set\HVWUTS.nml"
+copy /b "C:\Users\balin\OneDrive\Dokumentumok\GitHub\HVW-Urban-Trains-Set\Vehicles_code\*.nml" "C:\Users\balin\OneDrive\Dokumentumok\GitHub\HVW-Urban-Trains-Set\HVWUTS.nml"
+E:\python\python.exe version.py
+python3 E:\nml-master\nmlc -c --grf HVWUTS.grf HVWUTS.nml
 rd /s /q ".nmlcache\"
-copy "HVWUTS.grf" "C:\Users\balin\Documents\OpenTTD\content_download\newgrf"
+copy "HVWUTS.grf" "C:\Users\balin\OneDrive\Dokumentumok\OpenTTD\content_download\newgrf"
 pause>nul
